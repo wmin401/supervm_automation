0f55dd77aa571cfe67b70bd28ca6cb6b392a4281
dongill_lee <dongill_lee@tmax.co.kr>
2021-10-25 18:03:18 +0900

    클러스터 생성 삭제

--- __admin__/__cluster__.py
+++ __admin__/__cluster__.py
@@ -3,48 +3,61 @@ from selenium.webdriver.common.by import By
 import time
 
 class admin_cluster:
-    def __init__(self):
+    def __init__(self, webDriver):
         print("* 클러스터 테스트 시작")
         self._clusterResult = []
+        self._clusterName = 'auto_name'
+        self.webDriver = webDriver
         
-    def create(self, webDriver):
+    def create(self):
         print('1) 클러스터 생성')
         
         try:
             # 컴퓨팅
-            webDriver.implicitlyWait(10)
-            webDriver.findElement('id','compute',True)
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id','compute',True)
 
             # 호스트
-            webDriver.implicitlyWait(10)
-            webDriver.findElement('id','MenuView_clustersAnchor',True)
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id','MenuView_clustersAnchor',True)
 
             # 새로 만들기
-            webDriver.implicitlyWait(10)
-            webDriver.findElement('id','ActionPanelView_New',True)
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.explicitlyWait(10, By.ID, 'ClusterPopupView_nameEditor')
+            self.webDriver.findElement('id','ActionPanelView_New',True)
 
             # 이름 입력
-            #webDriver.implicitlyWait(30)
-            webDriver.explicitlyWait(10, By.ID, 'ClusterPopupView_nameEditor')
-            webDriver.findElement('id','ClusterPopupView_nameEditor',True)
-            webDriver.sendKey('auto_name')    
+            #self.webDriver.implicitlyWait(30)
+            self.webDriver.explicitlyWait(10, By.ID, 'ClusterPopupView_nameEditor')
+            self.webDriver.findElement('id','ClusterPopupView_nameEditor',True)
+            self.webDriver.sendKey(self._clusterName)    
             
             # 설명 입력
-            webDriver.implicitlyWait(40)
-            webDriver.findElement('id','ClusterPopupView_descriptionEditor',True)
-            webDriver.sendKey('auto_descrition')    
+            self.webDriver.implicitlyWait(40)
+            self.webDriver.findElement('id','ClusterPopupView_descriptionEditor',True)
+            self.webDriver.sendKey('auto_descrition')    
 
             # OK 버튼 클릭
-            webDriver.implicitlyWait(10)
-            webDriver.findElement('id','ClusterPopupView_OnSave',True)
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id','ClusterPopupView_OnSave',True)
             
             # 나중에 설정 버튼 클릭
-            webDriver.implicitlyWait(10)
-            webDriver.findElement('id','GuidePopupView_Cancel',True)
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id','GuidePopupView_Cancel',True)
             
+            
+            # tbody내부 전부 검색해서 입력한 이름이 있을경우 PASS
+            a = self.webDriver.tableSearch(self._clusterName)
+            
+            time.sleep(1)
 
-            result = PASS
-            msg = ''
+            if a == True:
+                result = PASS
+                msg = ''
+            else:
+                result = FAIL
+                msg = "Not created"
+
 
         except Exception as e:
             result = FAIL
@@ -53,3 +66,36 @@ class admin_cluster:
 
         print("* RESULT : " + result)
         self._clusterResult.append(['cluster;create;' + result + ';' + msg])
+
+    def remove(self):
+        try:
+            self.webDriver.tableSearch(self._clusterName, True)
+            time.sleep(1)
+
+            self.webDriver.findElement('css_selector','body > div.GHYIDY4CHUB > div.container-pf-nav-pf-vertical > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.toolbar-pf-actions > div:nth-child(2) > div > button')
+            self.webDriver.click()
+
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id','ActionPanelView_Remove',True)
+            
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id','RemoveConfirmationPopupView_OnRemove',True)
+            
+            a = self.webDriver.tableSearch(self._clusterName)
+                
+            time.sleep(1)
+
+            if a == True:
+                result = FAIL
+                msg = 'Not removed'
+            else:
+                result = PASS
+                msg = ''
+
+        except Exception as e:
+            result = FAIL
+            msg = str(e).replace("\n",'')
+            print("* MESSAGE : " + msg)
+
+        print("* RESULT : " + result)
+        self._clusterResult.append(['cluster;remove;' + result + ';' + msg])
