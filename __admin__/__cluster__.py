7e0a67eaa78e253818db71eb94e0cff29f2c8b01
dongill_lee <dongill_lee@tmax.co.kr>
2021-10-26 10:55:14 +0900

    출력 및 추석 추가

--- __admin__/__cluster__.py
+++ __admin__/__cluster__.py
@@ -8,7 +8,7 @@ class admin_cluster:
         self._clusterResult = []
         self._clusterName = 'auto_name'
         self.webDriver = webDriver
-        
+            
     def create(self):
         print('1) 클러스터 생성')
         
@@ -35,7 +35,7 @@ class admin_cluster:
             # 설명 입력
             self.webDriver.implicitlyWait(40)
             self.webDriver.findElement('id','ClusterPopupView_descriptionEditor',True)
-            self.webDriver.sendKey('auto_descrition')    
+            self.webDriver.sendKey('auto_description')    
 
             # OK 버튼 클릭
             self.webDriver.implicitlyWait(10)
@@ -45,18 +45,15 @@ class admin_cluster:
             self.webDriver.implicitlyWait(10)
             self.webDriver.findElement('id','GuidePopupView_Cancel',True)
             
+            # table 내부 전부 검색해서 입력한 이름이 있을경우 PASS
+            _createCheck = self.webDriver.tableSearch(self._clusterName)
             
-            # tbody내부 전부 검색해서 입력한 이름이 있을경우 PASS
-            a = self.webDriver.tableSearch(self._clusterName)
-            
-            time.sleep(1)
-
-            if a == True:
+            if _createCheck == True:
                 result = PASS
                 msg = ''
             else:
                 result = FAIL
-                msg = "Not created"
+                msg = "Failed to create new cluster..."
 
 
         except Exception as e:
@@ -68,26 +65,29 @@ class admin_cluster:
         self._clusterResult.append(['cluster;create;' + result + ';' + msg])
 
     def remove(self):
-        try:
+        try:            
+            # table 내부에 생성한 클러스터의 이름이 있을 경우 해당 row 클릭
             self.webDriver.tableSearch(self._clusterName, True)
             time.sleep(1)
 
-            self.webDriver.findElement('css_selector','body > div.GHYIDY4CHUB > div.container-pf-nav-pf-vertical > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.toolbar-pf-actions > div:nth-child(2) > div > button')
-            self.webDriver.click()
-
+            # 우측 추가 옵션 버튼 클릭
+            self.webDriver.implicitlyWait(10)            
+            self.webDriver.findElement('css_selector','body > div.GHYIDY4CHUB > div.container-pf-nav-pf-vertical > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.toolbar-pf-actions > div:nth-child(2) > div > button', True)
+            
+            # 삭제 버튼 클릭
             self.webDriver.implicitlyWait(10)
             self.webDriver.findElement('id','ActionPanelView_Remove',True)
             
+            # OK 버튼 클릭
             self.webDriver.implicitlyWait(10)
             self.webDriver.findElement('id','RemoveConfirmationPopupView_OnRemove',True)
             
-            a = self.webDriver.tableSearch(self._clusterName)
+            # table 내부 전부 검색해서 입력한 이름이 있을경우 FAIL
+            _removeCheck = self.webDriver.tableSearch(self._clusterName)
                 
-            time.sleep(1)
-
-            if a == True:
+            if _removeCheck == True:
                 result = FAIL
-                msg = 'Not removed'
+                msg = 'Failed to remove cluster'
             else:
                 result = PASS
                 msg = ''
