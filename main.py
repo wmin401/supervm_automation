00f10984363df63c389be69fc692457993d47c8b
dongill_lee <dongill_lee@tmax.co.kr>
2021-10-28 12:49:49 +0900

    출력문 수정

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     webDriver.implicitlyWait(5)
     _supervmVersionElem = webDriver.findElement('css_selector','body > main > section > div.pf-l-split > div.pf-l-split__item.obrand_welcomePageVersionText')
     _supervmVersion = _supervmVersionElem.get_attribute('textContent')
-    printLog('SuperVM Ver : ' + _supervmVersion[_supervmVersion.find('4'):_supervmVersion.find('prolinux8')+9])
+    printLog('* SuperVM Ver : ' + _supervmVersion[_supervmVersion.find('4'):_supervmVersion.find('prolinux8')+9])
     
 
     printLine()
