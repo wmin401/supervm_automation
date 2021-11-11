9dd4597f8f5a51d039c3d7ded06377ea4d9f5b4d
dongill_lee <dongill_lee@tmax.co.kr>
2021-11-11 12:44:27 +0900

    젠킨스에서만 junit 파일 생성되도록 변경

--- __common__/__testlink__.py
+++ __common__/__testlink__.py
@@ -13,28 +13,29 @@ class testlink:
             os.makedirs(RESULT_PATH + '/'+ self.junitsFolder)
 
     def junitBuilder(self, *args):
-        args = list(args)   
-        testsuite = Element('testsuite')
-        testcase = SubElement(testsuite, 'testcase')
-        testcase.set('classname', args[0])
-        testcase.set('name', args[0])
-        testcase.set('status',args[1].lower()+'ed')
-        if args[1] == FAIL:
-            failure = SubElement(testcase, 'failure')
-            failure.text = args[2]
-        else: ## pass or blocked
-            testcase.text = args[2]
+        ## 젠킨스 에서만 생성되도록 변경
+        if IN_JENKINS == 'true':
+            args = list(args)   
+            testsuite = Element('testsuite')
+            testcase = SubElement(testsuite, 'testcase')
+            testcase.set('classname', args[0])
+            testcase.set('name', args[0])
+            testcase.set('status',args[1].lower()+'ed')
+            if args[1] == FAIL:
+                failure = SubElement(testcase, 'failure')
+                failure.text = args[2]
+            else: ## pass or blocked
+                testcase.text = args[2]
 
-        ## block인 경우엔 testNG 파일이 필요한 상태
-
-        tree = ElementTree(testsuite)
-        filePath = RESULT_PATH + '/' + self.junitsFolder+ '/junit-'+str(args[0])+'.xml'
-        try:
-            tree.write(filePath,encoding="utf-8", xml_declaration=True)
-            print("* junit file is successfully created!")
-        except Exception as e:            
-            print("*** Junit Build Exception : %s"%(e))
-            return
+            ## block인 경우엔 testNG 파일이 필요한 상태
+            tree = ElementTree(testsuite)
+            filePath = RESULT_PATH + '/' + self.junitsFolder+ '/junit-'+str(args[0])+'.xml'
+            try:
+                tree.write(filePath,encoding="utf-8", xml_declaration=True)
+                print("* junit file is successfully created!")
+            except Exception as e:            
+                print("*** Junit Build Exception : %s"%(e))
+                return
 
 
 # junit-커스텀필드명.xml 파일 
