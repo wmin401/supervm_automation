285c5f68d3cfb87851068a6b476174c9488a5c47
dongill_lee <dongill_lee@tmax.co.kr>
2021-11-12 10:23:16 +0900

    import 파일 생성

---
+++ __import__.py
@@ -0,0 +1,16 @@
+# 공통 파일
+from __common__.__driver__ import *
+from __common__.__parameter__ import *
+from __common__.__login__ import *
+from __common__.__portal__ import *
+from __common__.__csv__ import *
+from __common__.__testlink__ import *
+
+# 테스트 파일
+from __test__.__admin__.__host__ import *
+from __test__.__admin__.__cluster__ import *
+from __test__.__admin__.__data_center__ import *
+from __test__.__admin__.__disk__ import *
+from __test__.__admin__.__domain__ import *
+from __test__.__admin__.__qos__ import *
+from __test__.__vm__.__vm__ import *
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-# 공통 파일
-from __common__.__driver__ import *
-from __common__.__parameter__ import *
-from __common__.__login__ import *
-from __common__.__portal__ import *
-from __common__.__csv__ import *
 
-# 테스트 파일
-from __test__.__admin__.__host__ import *
-from __test__.__admin__.__cluster__ import *
-from __test__.__admin__.__data_center__ import *
-from __test__.__admin__.__disk__ import *
-from __test__.__admin__.__domain__ import *
-from __test__.__admin__.__qos__ import *
-from __test__.__vm__.__vm__ import *
+from __import__ import * 
 
 ## 테스트 메인 파일
 ## 여기서 각 파일들을 불러와서 테스트 진행
