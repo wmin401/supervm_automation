6ea54c59d06a1ac7e918571b1c54a27b5dcb312d
dongill_lee <dongill_lee@tmax.co.kr>
2021-12-01 11:29:55 +0900

    테스트코드 업로드

--- __deploy__.py
+++ __deploy__.py
@@ -4,12 +4,13 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(sys.argv[1], username=sys.argv[3], password=sys.argv[4], port=sys.argv[2])
+#ssh.connect('192.168.17.161',username='root', password='asdf', port='22')
 
 stdin, stdout, stderr = ssh.exec_command('dos2unix /root/deploy_automation.sh', timeout=10)        
 output = stdout.readlines()
 for i in output:
     print(i.replace('\n',''))
-stdin, stdout, stderr = ssh.exec_command('sh /root/deploy_automation.sh', timeout=10)        
+stdin, stdout, stderr = ssh.exec_command('sh /root/deploy_automation.sh %s %s'%(sys.argv[5],sys.argv[6]), timeout=10)        
 output = stdout.readlines()
 for i in output:
     print(i.replace('\n',''))
