f73852e23cddc894f3f1601060824930240e0690
jeonghyeon_kim <jeonghyeon_kim@tmax.co.kr>
2021-12-29 16:49:05 +0900

    풀 생성 코드 작성
    - 풀 생성 부분 초안 완료

--- __test__/__admin__/__pools__.py
+++ __test__/__admin__/__pools__.py
@@ -7,7 +7,7 @@ from __common__.__testlink__ import *
 
 from selenium.webdriver.common.by import By
 
-from __test__.__admin__ import *
+from __test__.__admin__.__template__ import *
 
 '''
     작성자 : CQA2 김정현
@@ -17,104 +17,82 @@ class admin_pools:
     def __init__(self, webDriver):
         self._poolsResult = []
         self.webDriver = webDriver
-        self._templateName = 'auto_template_'+ randomString()
+        self._poolsName = 'auto_pools_'+ randomString()
         self.tl = testlink()
-        # class import 부분이 안되고 있음
-        # _admin_template_instance = admin_template(webDriver)
+        self._template_instance = admin_template(webDriver) # template class 사용
 
     def test(self):
-        # _admin_template_instance.setup()
-        # _admin_template_instance.create()
-        self.template_setup() # 템플릿 메뉴 접근 확인
-        self.template_create() # 템플릿 생성
+        self._template_instance.setup() # 템플릿 메뉴 접근 확인
+        self._template_instance.create() # 템플릿 생성
         time.sleep(0.3)
+        self.setup()
+        self.create()
 
-    def template_setup(self):
-        # 템플릿 메뉴 접근
+    def setup(self):
+        # 풀 메뉴 접근
 
         # 컴퓨팅 클릭
-        printLog("[POOL TEMPLATE_CREATE] Compute - Template ")
+        printLog("[SETUP] Compute - Pools ")
         self.webDriver.implicitlyWait(10)
         self.webDriver.findElement('id','compute',True)
 
-        # 템플릿 클릭
-        self.webDriver.explicitlyWait(10, By.ID, 'MenuView_templatesAnchor')
-        self.webDriver.findElement('id','MenuView_templatesAnchor',True)
+        # 풀 클릭
+        self.webDriver.explicitlyWait(10, By.ID, 'MenuView_poolsAnchor')
+        self.webDriver.findElement('id','MenuView_poolsAnchor',True)
 
         time.sleep(2)
 
-    def template_create(self):
-        printLog(printSquare('Create Template'))
+    def create(self):
+        printLog(printSquare('Create Pools'))
         try:
             result = FAIL
             msg = ''
-            # 중지된 가상머신이 있어야한다.
-            # 컴퓨팅 클릭
-            printLog("[POOL CREATE TEMPLATE] Compute - Virtual Machines ")
+            # 풀 새로 만들기 버튼 클릭
+            printLog("[CREATE POOLS] Create pools")
             self.webDriver.implicitlyWait(10)
-            self.webDriver.findElement('id','compute',True)
+            self.webDriver.findElement('id','ActionPanelView_New',True)
 
-            # 가상머신 클릭
-            self.webDriver.implicitlyWait(10)
-            self.webDriver.findElement('id','MenuView_vmsAnchor',True)
-            self.webDriver.tableSearch('Down', 13, rowClick=True)
-            time.sleep(0.3)
-
-            # 추가 옵션 버튼 클릭
-            self.webDriver.implicitlyWait(10)
-            self.webDriver.findElement('css_selector','body > div.GHYIDY4CHUB > div.container-pf-nav-pf-vertical > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.toolbar-pf-actions > div:nth-child(2) > div.btn-group.dropdown-kebab-pf.dropdown.pull-right > button',True)
-
-            # 템플릿 생성 클릭
-            printLog("[POOL CREATE TEMPLATE] Create template")
-            self.webDriver.implicitlyWait(10)
-            self.webDriver.findElement('id','ActionPanelView_NewTemplate',True)
-
-            # 템플릿 이름 입력
-            self.webDriver.explicitlyWait(10, By.ID, 'VmMakeTemplatePopupWidget_name')
-            self.webDriver.findElement('id','VmMakeTemplatePopupWidget_name')
-            self.webDriver.sendKeys(self._templateName)
-            printLog("[POOL CREATE TEMPLATE] Template name : %s"%self._templateName)
-
-            # 가상 머신 권한 복사 체크
-            self.webDriver.implicitlyWait(10)
-            self.webDriver.findElement('id','VmMakeTemplatePopupWidget_copyVmPermissions', True)
+            # 풀 이름 입력
+            self.webDriver.explicitlyWait(10, By.ID, 'PoolNewPopupWidget_name')
+            self.webDriver.findElement('id','PoolNewPopupWidget_name')
+            time.sleep(3)
+            self.webDriver.sendKeys(self._poolsName)
+            printLog("[CREATE POOLS] Pools name : %s"%self._poolsName)
 
             # OK 클릭
             self.webDriver.implicitlyWait(10)
-            self.webDriver.findElement('id','VmMakeTemplatePopupView_OnNewTemplate', True)
+            self.webDriver.findElement('id','PoolNewPopupView_OnSave', True)
             time.sleep(3)
-            # 템플릿 탭
-            self.template_setup()
 
-            printLog("[POOL CREATE TEMPLATE] Check if created")
-            printLog("[POOL CREATE TEMPLATE] Wait until status will be OK")
+            printLog("[CREATE POOLS] Check if created")
+            printLog("[CREATE POOLS] Wait until status will be OK")
             # table 내부 전부 검색해서 입력한 이름이 있을경우 PASS
-            # _createCheck = self.webDriver.tableSearch(self._templateName, 1) # 템플릿 테이블에 숨겨진 열(0)이 하나 있어서 1부터 시작
-            st = time.time()
+            _startTime = time.time()
             while True:
                 time.sleep(1)
                 try:
-                    tableValueList = self.webDriver.tableSearch(self._templateName, 1, rowClick=False, nameClick=False, returnValueList=True)        
-                    if '잠김' in tableValueList[5] or 'Locked' in tableValueList[5]:
-                        printLog("[CREATE VM] Template's status is still locked ...")
-                        continue
-                    elif 'OK' in tableValueList[5]:
+                    tableValueList = self.webDriver.tableSearch(self._poolsName, 0, rowClick=False, nameClick=False, returnValueList=True)
+                    if self._poolsName in tableValueList[0]:
                         result = PASS
                         msg = ''
                         break
-                    ed = time.time()
-                    if ed-st >= 60:
-                        printLog("[CREATE VM] Failed status changed : Timeout")
+                    elif self._poolsName not in tableValueList[0]:
+                        printLog("[CREATE POOLS] Pools status is still created ...")
+                        continue
+                    _endTime = time.time()
+                    if _endTime - _startTime >= 60:
+                        printLog("[CREATE POOLS] Failed status changed : Timeout")
                         result = FAIL
-                        msg = "Failed to create new template..."
+                        msg = "Failed to create new pools..."
                         break
                 except:
                     continue
+
         except Exception as e:
             result = FAIL
             msg = str(e).replace("\n",'')
-            printLog("[POOL CREATE TEMPLATE] MESSAGE : " + msg)
-        printLog("[POOL CREATE TEMPLATE] RESULT : " + result)
-        self._poolsResult.append(['pool template' + DELIM + 'template create' + DELIM + result + DELIM + msg])
+            printLog("[CREATE POOLS] MESSAGE : " + msg)
+        printLog("[CREATE POOLS] RESULT : " + result)
+        self._poolsResult.append(['pools' + DELIM + 'create' + DELIM + result + DELIM + msg])
 
-        self.tl.junitBuilder('POOL_TEMPLATE_CREATE', result, msg)
+        self.tl.junitBuilder('POOLS_CREATE', result, msg)
