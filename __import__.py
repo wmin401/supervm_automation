d5e64247f49a3d045c23ea2b3b89bbed430c3dc3
jaewon_kim2 <jaewon_kim2@tmax.co.kr>
2022-01-05 18:01:19 +0900

    Update __import__.py

--- __import__.py
+++ __import__.py
@@ -20,5 +20,6 @@ from __test__.__admin__.__vm2__ import *
 from __test__.__admin__.__pools__ import *
 from __test__.__admin__.__external_provider__ import *
 from __test__.__vm__.__vm__ import *
+from __test__.__admin__.__quota__ import *
 
 # entry point import
