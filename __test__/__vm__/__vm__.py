bcd1e7b070f8001c55f89023dcc6365d083d3a4b
dongill_lee <dongill_lee@tmax.co.kr>
2022-01-05 16:14:50 +0900

    코드 4.4.9버전에 맞춰서 수정

--- __test__/__vm__/__vm__.py
+++ __test__/__vm__/__vm__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from random import weibullvariate
 from __common__.__parameter__ import *
 from __common__.__module__ import *
 from __common__.__testlink__ import *
@@ -13,7 +15,7 @@ class vm_vm:
         self.tl = testlink()
 
     def test(self):
-        # self.create() # 가상머신 생성 코드 문제로 인해 보류
+        self.create() # 가상머신 생성 코드 문제로 인해 보류
         self.detail()
         self.update()
         self.remove()
@@ -21,79 +23,124 @@ class vm_vm:
     def create(self):
         self.tl.junitBuilder('VM_PORTAL_LOGIN', PASS, '') # VM 포털 로그인 
         
-        printLog('- Create VM')
+        printLog(printSquare('Create VM in Portal'))
         result = FAIL
         msg = ''
 
 
         try:
-            # 가상 머신 생성 버튼 클릭
+            time.sleep(1)
+            # click
             self.webDriver.explicitlyWait(10, By.ID, 'route-add-vm-button')
-            self.webDriver.findElement('id','route-add-vm-button', True)
-            # 이름 입력            
-            self.webDriver.findElement('id','create-vm-wizard-basic-name-edit')
+            self.webDriver.findElement('id', 'route-add-vm-button', True)
+            time.sleep(0.5)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-name-edit')
+            self.webDriver.findElement('id', 'create-vm-wizard-basic-name-edit', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-name-edit')
+            self.webDriver.findElement('id', 'create-vm-wizard-basic-name-edit', False)
             self.webDriver.sendKeys(self._vmName)
-            # 클러스터 클릭 및 Default 선택
-            self.webDriver.findElement('id','create-vm-wizard-basic-cluster-edit-button-toggle', True)
-            self.webDriver.findElement('css_selector','#create-vm-wizard-basic-cluster-edit > div > ul > li:nth-child(1)')
-            a = self.webDriver.getAttribute('textContent')
-            
-            
 
-            # 프로비저닝 소스 클릭 및 템플릿 선택
-            self.webDriver.findElement('id','create-vm-wizard-basic-provision-edit-button-toggle', True)
+            # click
+            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-description-edit')
+            self.webDriver.findElement('id', 'create-vm-wizard-basic-description-edit', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-description-edit')
+            self.webDriver.findElement('id', 'create-vm-wizard-basic-description-edit', False)
+            self.webDriver.sendKeys('automated creation')
+
+            # 클러스터 click
+            self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-cluster-edit-button-toggle').click()")
+            time.sleep(0.1)
+
+            # 메뉴 중 Default 클릭
+            self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-cluster-edit-item-Default (Default)').click()")
+            time.sleep(0.1)
+            
+            # 프로비저닝 소스 클릭
+            self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-provision-edit-button-toggle').click()")
+            time.sleep(0.1)
+
+            # 메뉴 중 템플릿 클릭
             try:
-                # 한글일 경우
-                self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-provision-edit-item-템플릿')
-                self.webDriver.findElement('id','create-vm-wizard-basic-provision-edit-item-템플릿', True)
-            except: 
-                # 영문일 경우
-                self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-provision-edit-item-Template')
-                self.webDriver.findElement('id','create-vm-wizard-basic-provision-edit-item-Template', True)                
-            # 템플릿 클릭 및 Blank 선택
-            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-template-edit-button-text')
-            self.webDriver.findElement('id','create-vm-wizard-basic-template-edit-button-text', True)
-            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-basic-template-edit-item-Blank')
-            self.webDriver.findElement('id','create-vm-wizard-basic-template-edit-item-Blank', True)
-            # 다음 클릭
-            self.webDriver.findElement('css_selector', '.btn:nth-child(3)', True)
-            # NIC 생성
-            self.webDriver.findElement('css_selector', '#create-vm-wizards-net > div > div.blank-slate-pf-main-action > button', True)
-            self.webDriver.findElement('css_selector', '#create-vm-wizards-net > div._2w_sbU1I-QKEdNDpIvRpri > div > button.btn.btn-primary', True)
-            # 다음 클릭
-            self.webDriver.findElement('css_selector', 'body > div:nth-child(3) > div.fade.in.modal > div > div > div > div.wizard-pf-footer.modal-footer > button.btn.btn-primary', True)
-            # 디스크 생성
-            self.webDriver.findElement('css_selector', '#create-vm-wizards-storage > div > div.blank-slate-pf-main-action > button', True)
-            self.webDriver.findElement('css_selector', '#create-vm-wizards-storage > div._5dFBDOu9x8heSWkSiAvka > div > button.btn.btn-primary', True)
-            # 다음 클릭
-            self.webDriver.findElement('css_selector', 'body > div:nth-child(3) > div.fade.in.modal > div > div > div > div.wizard-pf-footer.modal-footer > button.btn.btn-primary', True)
-            # 가상 머신 생성 클릭
-            self.webDriver.findElement('css_selector', 'body > div:nth-child(3) > div.fade.in.modal > div > div > div > div.wizard-pf-footer.modal-footer > button.btn.btn-primary', True)
-            # 생성 확인
-            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizard-review-review-progress-success')            
-            self.webDriver.findElement('id','create-vm-wizard-review-review-progress-success')
-            #print(self.webDriver.getAttribute('textContent'))
+                self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-provision-edit-item-템플릿').click()")
+            except:
+                self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-provision-edit-item-Template').click()")            
+            time.sleep(0.1)
 
-            time.sleep(2)
+            # 템플릿 클릭
+            self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-template-edit-button-toggle').click()")
+            time.sleep(0.1)
+
+            # Blank 클릭
+            self.webDriver.executeScript("document.getElementById('create-vm-wizard-basic-template-edit-item-Blank').click()")
+            time.sleep(0.1)
+
+            # click
+            self.webDriver.findElement('xpath', '/html/body/div[2]/div[2]/div/div/div/div[3]/button[3]', True)
+            time.sleep(0.1)
+
+            # click            
+            self.webDriver.findElement('css_selector', '#create-vm-wizards-net > div > div.blank-slate-pf-main-action > button', True)
+            time.sleep(0.1)
+
+            # click
+            self.webDriver.findElement('css_selector', '#create-vm-wizards-net > div.src-components-CreateVmWizard-steps-style__nic-table--1korfGa-KL > div > button.btn.btn-primary', True)
+            time.sleep(0.1)
+
+            # click
+            self.webDriver.findElement('xpath', '/html/body/div[2]/div[2]/div/div/div/div[3]/button[3]', True)
+            time.sleep(0.1)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '#create-vm-wizards-storage > div > div.blank-slate-pf-main-action > button')
+            self.webDriver.findElement('css_selector', '#create-vm-wizards-storage > div > div.blank-slate-pf-main-action > button', True)
+            time.sleep(0.1)
+
+            # type
+            
+            self.webDriver.explicitlyWait(10, By.ID, 'create-vm-wizards-storage-undefined-size-edit')
+            self.webDriver.findElement('id', 'create-vm-wizards-storage-undefined-size-edit', True)
+            self.webDriver.sendKeys('1')
+            time.sleep(0.3)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '#create-vm-wizards-storage > div.src-components-CreateVmWizard-steps-style__disk-table--2onYAmcpqP > div > button.btn.btn-primary')
+            self.webDriver.findElement('css_selector', '#create-vm-wizards-storage > div.src-components-CreateVmWizard-steps-style__disk-table--2onYAmcpqP > div > button.btn.btn-primary', True)
+            time.sleep(0.3)
+
+            # click
+            self.webDriver.findElement('xpath', '/html/body/div[2]/div[2]/div/div/div/div[3]/button[3]', True)
+            time.sleep(0.1)
+
+            # click
+            self.webDriver.findElement('xpath', '/html/body/div[2]/div[2]/div/div/div/div[3]/button[3]', True)
+            time.sleep(5)
+   
+            self.webDriver.findElement('css_selector','#create-vm-wizard-review-review-progress-success > div.src-components-CreateVmWizard-steps-style__review-text--2hWB4OczhF')
             if 'SUCCESS' in self.webDriver.getAttribute('textContent') or '성공' in self.webDriver.getAttribute('textContent'):
                 result = PASS
                 msg = self.webDriver.getAttribute('textContent')
-                printLog("* VM NAME : %s"%self._vmName)
+                printLog("[PORTAL VM CREATE] VM NAME : %s"%self._vmName)
 
-            self.webDriver.findElement('css_selector', 'body > div:nth-child(3) > div.fade.in.modal > div > div > div > div.wizard-pf-footer.modal-footer > button:nth-child(3)', True)
+            self.webDriver.findElement('xpath', '/html/body/div[2]/div[2]/div/div/div/div[3]/button[3]', True)
 
         except Exception as e:
             result = FAIL
             msg = str(e).replace("\n",'')
             msg = msg[:msg.find('Element <')]
-        printLog("* RESULT : " + result)
+        printLog("[PORTAL VM CREATE] RESULT : " + result)
         self._vmPortalResult.append(['vm portal' + DELIM + 'vm create' + DELIM + result + DELIM + msg])
         
         self.tl.junitBuilder('VM_PORTAL_VM_CREATE', result, msg)
 
     def detail(self):
 
-        printLog('- Detail VM')
+        printLog(printSquare('Detail VM in Portal'))
         result = FAIL
         msg = ''
 
@@ -111,7 +158,7 @@ class vm_vm:
             msg = str(e).replace("\n",'')
             msg = msg[:msg.find('Element <')]
         
-        printLog("* RESULT : " + result)
+        printLog("[PORTAL VM DETAIL] RESULT : " + result)
         self._vmPortalResult.append(['vm portal' + DELIM + 'vm detail' + DELIM + result + DELIM + msg])
         
         self.tl.junitBuilder('VM_PORTAL_VM_DETAIL', result, msg)
@@ -119,7 +166,7 @@ class vm_vm:
         self.webDriver.findElement('id','breadcrumb-link-0', True) 
 
     def update(self):        
-        printLog('- Update VM')
+        printLog(printSquare('Update VM in Portal'))
         result = FAIL
         msg = ''
 
@@ -135,6 +182,7 @@ class vm_vm:
             self.webDriver.clear()
             self.webDriver.sendKeys(Keys.CONTROL + "a")
             self.webDriver.sendKeys(self._vmCPUs)
+            printLog("[PORTAL VM UPDATE] Change VM's CPU 1 to 2")
             self.webDriver.findElement('id','vmdetail-details-button-save', True)
             time.sleep(1)
 
@@ -147,7 +195,7 @@ class vm_vm:
             result = FAIL
             msg = str(e).replace("\n",'')
             msg = msg[:msg.find('Element <')]
-        printLog("* RESULT : " + result)
+        printLog("[PORTAL VM UPDATE] RESULT : " + result)
         self._vmPortalResult.append(['vm portal' + DELIM + 'vm update' + DELIM + result + DELIM + msg])
         
         self.tl.junitBuilder('VM_PORTAL_VM_UPDATE', result, msg)
@@ -156,42 +204,33 @@ class vm_vm:
         self.webDriver.findElement('id','breadcrumb-link-0', True) 
 
     def remove(self):
-        printLog('* Wait 30 seconds')
-        time.sleep(30)
+        printLog('[PORTAL VM REMOVE] Wait 15 seconds until the disk is unlocked.')
+        time.sleep(15)
 
-        printLog('- Remove VM')
+        printLog(printSquare('Remove VM in Portal'))
         result = FAIL
         msg = ''
 
         try: 
             # 생성한 VM 이름 클릭
-            self.webDriver.findElement('id','vm-' + self._vmName + '-name', True)       
+            self.webDriver.findElement('id','vm-' + self._vmName + '-name', True)   
+            time.sleep(0.3)    
             # 삭제 버튼 클릭
             self.webDriver.explicitlyWait(10, By.ID, 'vmaction-' + self._vmName + '-actions-button-remove-title')
             self.webDriver.findElement('id', 'vmaction-' + self._vmName + '-actions-button-remove-title', True)
             # 디스크 유지 클릭
             self.webDriver.findElement('css_selector', '#vmaction-' + self._vmName + '-actions-preservedisks > div > label > input', True)
             # 삭제 버튼 클릭
-            self.webDriver.findElement('css_selector', 'body > div:nth-child(3) > div.fade.message-dialog-pf.in.modal > div > div > div.modal-footer > button.btn.btn-danger', True)
-            # 삭제 확인
-            time.sleep(10)
-            try:
-                self.webDriver.findElement('id','vm-' + self._vmName + '-name')
-                msg = 'Remove failed'
-            except Exception as e:
-                msg = str(e).replace("\n",'')
-                msg = msg[:msg.find('Element <')]
-                if "no such element" in msg.lower():
-                    result = PASS
-                    msg = ''
-                else:
-                    result = FAIL
+            self.webDriver.findElement('xpath', '/html/body/div[2]/div[2]/div/div/div[3]/button[2]', True)
+            
+            result = PASS
+            msg = ''
             
         except Exception as e:
             result = FAIL
             msg = str(e).replace("\n",'')
             msg = msg[:msg.find('Element <')]
-        printLog("* RESULT : " + result)
+        printLog("[PORTAL VM REMOVE] RESULT : " + result)
         self._vmPortalResult.append(['vm portal' + DELIM + 'vm remove' + DELIM + result + DELIM + msg])
         
         self.tl.junitBuilder('VM_PORTAL_VM_REMOVE', result, msg)
