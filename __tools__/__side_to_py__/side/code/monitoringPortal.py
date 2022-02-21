a0c73d9bee64df3f24d28cc89f0ebad0cc7a36ca
woonggi_min@tmax.co.kr <woonggi_min@tmax.co.kr>
2022-02-21 15:29:28 +0900

    woonggi 02/21/1

--- __tools__/__side_to_py__/side/code/monitoringPortal.py
+++ __tools__/__side_to_py__/side/code/monitoringPortal.py
@@ -10,16 +10,90 @@
         try:
 
             # click
-            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '#compute > .list-group-item-value')
-            self.webDriver.findElement('css_selector', '#compute > .list-group-item-value', True)
+            self.webDriver.explicitlyWait(10, By.NAME, 'user')
+            self.webDriver.findElement('name', 'user', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.NAME, 'user')
+            self.webDriver.findElement('name', 'user', False)
+            self.webDriver.sendKeys('') # You have to change this you want to write
+
+            # type
+            self.webDriver.explicitlyWait(10, By.NAME, 'password')
+            self.webDriver.findElement('name', 'password', False)
+            self.webDriver.sendKeys('') # You have to change this you want to write
 
             # click
-            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '#MenuView_clustersAnchor > .list-group-item-value')
-            self.webDriver.findElement('css_selector', '#MenuView_clustersAnchor > .list-group-item-value', True)
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '.css-1daj7gy-button')
+            self.webDriver.findElement('css_selector', '.css-1daj7gy-button', True)
 
             # click
-            self.webDriver.explicitlyWait(10, By.ID, 'MainClusterView_table_content_col1_row0')
-            self.webDriver.findElement('id', 'MainClusterView_table_content_col1_row0', True)
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '.sidemenu-item')
+            self.webDriver.findElement('css_selector', '.sidemenu-item', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.LINK_TEXT, 'Users')
+            self.webDriver.findElement('link_text', 'Users', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '.css-1mhnkuh')
+            self.webDriver.findElement('css_selector', '.css-1mhnkuh', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.NAME, 'name')
+            self.webDriver.findElement('name', 'name', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.NAME, 'name')
+            self.webDriver.findElement('name', 'name', False)
+            self.webDriver.sendKeys('') # You have to change this you want to write
+
+            # click
+            self.webDriver.explicitlyWait(10, By.NAME, 'email')
+            self.webDriver.findElement('name', 'email', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.NAME, 'email')
+            self.webDriver.findElement('name', 'email', False)
+            self.webDriver.sendKeys('') # You have to change this you want to write
+
+            # click
+            self.webDriver.explicitlyWait(10, By.NAME, 'login')
+            self.webDriver.findElement('name', 'login', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.NAME, 'login')
+            self.webDriver.findElement('name', 'login', False)
+            self.webDriver.sendKeys('') # You have to change this you want to write
+
+            # click
+            self.webDriver.explicitlyWait(10, By.NAME, 'password')
+            self.webDriver.findElement('name', 'password', True)
+
+            # type
+            self.webDriver.explicitlyWait(10, By.NAME, 'password')
+            self.webDriver.findElement('name', 'password', False)
+            self.webDriver.sendKeys('') # You have to change this you want to write
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '.css-1mhnkuh')
+            self.webDriver.findElement('css_selector', '.css-1mhnkuh', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '.sidemenu-item')
+            self.webDriver.findElement('css_selector', '.sidemenu-item', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.LINK_TEXT, 'Users')
+            self.webDriver.findElement('link_text', 'Users', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, 'tr')
+            self.webDriver.findElement('css_selector', 'tr', True)
+
+            # click
+            self.webDriver.explicitlyWait(10, By.CSS_SELECTOR, '.css-jigxr0-button > .css-1mhnkuh')
+            self.webDriver.findElement('css_selector', '.css-jigxr0-button > .css-1mhnkuh', True)
    
         except Exception as e:   
             result = FAIL
