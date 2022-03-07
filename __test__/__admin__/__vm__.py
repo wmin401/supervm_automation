c41bae7a9d60380362233cc9c9ad2db72aaeb916
dongill_lee@tmax.co.kr <dongill_lee@tmax.co.kr>
2022-03-07 18:01:11 +0900

    코드 수정

--- __test__/__admin__/__vm__.py
+++ __test__/__admin__/__vm__.py
@@ -76,25 +76,25 @@ class admin_vm:
         # VM 생성 - 2
         time.sleep(5)
         self.create()
-        # self.createWindows()
+        self.createWindows()
 
         # 가상 디스크 - 4
-        # self.addVirtualDisk()
-        # self.attachDisk()
-        # # self.virtualDiskHotPlugging()
-        # self.removeVirtualDisk()
+        self.addVirtualDisk()
+        self.attachDisk()
+        # self.virtualDiskHotPlugging()
+        self.removeVirtualDisk()
 
         # 네트워크 인터페이스 - 4
-        # # self.addNetworkInterface()
-        # # self.updateNetworkInterface()
-        # # self.networkInterfaceHotPlugging()
-        # # self.deleteNetworkInterface()
+        # self.addNetworkInterface()
+        # self.updateNetworkInterface()
+        # self.networkInterfaceHotPlugging()
+        # self.deleteNetworkInterface()
 
         # # # 업데이트 - 1
-        # self.update()
+        self.update()
 
-        # # # 복사 - 1 
-        # self.copy()
+        # # 복사 - 1 
+        self.copy()
 
         # 실행 - 1
         self.run()
@@ -527,7 +527,7 @@ class admin_vm:
             # self.webDriver.tableSearch('auto_vm_HnpZbEOS', 2, True)
             self.webDriver.tableSearch(self._vmName, 2, True)
             self.webDriver.findElement('css_selector','#ActionPanelView_Shutdown > button:nth-child(1)',True)
-            time.sleep(1)
+            time.sleep(3)
                 # OK 클릭
             try:
                 self.webDriver.findElement('css_selector','#RemoveConfirmationPopupView_OnShutdown > button',True)
@@ -538,13 +538,11 @@ class admin_vm:
             st = time.time()
             before = ''
             t = 300
-            failLst = ['Up', '전원을 끄는 중']
-            passLst = ['Down']
             while True:
                 ed = time.time()
                 if result == PASS:
                     break
-                if ed-st >= 300:
+                if ed-st >= t:
                     printLog("[%s STATUS] Failed status changed : %ss Timeout"%(self._vmName,t))
                     result, msg = FAIL, 'Timeout'
                     break
@@ -553,19 +551,19 @@ class admin_vm:
                     tableValueList = self.webDriver.tableSearch(self._vmName, 2, rowClick=False, nameClick=False, returnValueList=True)    
                     printLog(tableValueList, debug=True)    
                     current = tableValueList[13]
-                    for failStr in failLst:
+                    for failStr in ['Up', '전원을 끄는 중']:
                         if failStr in tableValueList[13]:
                             if current != before:
                                 printLog("[%s STATUS] %s"%(self._vmName, tableValueList[13]))
                                 before = current
 
-                    for passStr in passLst:
+                    for passStr in ['Down']:
                         if passStr == tableValueList[13]:
                             printLog("[%s STATUS] %s"%(self._vmName, tableValueList[13]))
                             result, msg = PASS, ''
                             break
                 except Exception as e: 
-                    printLog('[STATUS CHECK] %s'%(str(e)))
+                    printLog('[STATUS CHECK EXCEPTION] %s'%(str(e)))
                     continue
             
 
