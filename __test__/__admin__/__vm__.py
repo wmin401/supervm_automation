efd16435c3922dd43ea22ac6af24c1f53e4d2c88
dongill_lee@tmax.co.kr <dongill_lee@tmax.co.kr>
2022-03-10 16:13:37 +0900

    젠킨스 테스트용 주석처리

--- __test__/__admin__/__vm__.py
+++ __test__/__admin__/__vm__.py
@@ -76,45 +76,45 @@ class admin_vm:
         # VM 생성 - 2
         time.sleep(5)
         self.create()
-        self.createWindows()
+        # self.createWindows()
 
-        # 가상 디스크 - 4
-        self.addVirtualDisk()
-        self.attachDisk()
-        # self.virtualDiskHotPlugging()
-        self.removeVirtualDisk()
+        # # 가상 디스크 - 4
+        # self.addVirtualDisk()
+        # self.attachDisk()
+        # # self.virtualDiskHotPlugging()
+        # self.removeVirtualDisk()
 
-        # 네트워크 인터페이스 - 4
-        self.addNetworkInterface()
-        self.updateNetworkInterface()
-        self.networkInterfaceHotPlugging()
-        self.deleteNetworkInterface()
+        # # 네트워크 인터페이스 - 4
+        # self.addNetworkInterface()
+        # self.updateNetworkInterface()
+        # self.networkInterfaceHotPlugging()
+        # self.deleteNetworkInterface()
 
-        # # # 업데이트 - 1
-        self.update()
+        # # # # 업데이트 - 1
+        # self.update()
 
-        # # 복사 - 1 
-        self.copy()
+        # # # 복사 - 1 
+        # self.copy()
 
         # 실행 - 1
         self.run()
 
         # 호스트 - 2
-        self.pinToMultipleHosts()
-        self.ViewingPinnedToHost()
+        # self.pinToMultipleHosts()
+        # self.ViewingPinnedToHost()
 
-        # 가상 메모리 - 2
-        self.virtualMemoryHotPlugging()
-        # self.virtualMemoryHotUnplugging()
+        # # 가상 메모리 - 2
+        # self.virtualMemoryHotPlugging()
+        # # self.virtualMemoryHotUnplugging()
 
-        # vcpu - 1
-        self.hotPluggingVCPU()
+        # # vcpu - 1
+        # self.hotPluggingVCPU()
 
-        # cd변경 - 1
-        self.changeCD()
+        # # cd변경 - 1
+        # self.changeCD()
 
         # 종료 및 정지 - 4
-        self.reboot()
+        # self.reboot()
         self.pause()
         self.shutdown()
         self.remove()
