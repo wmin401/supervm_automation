8f196dd885d5ed0a68e405edbce161495127af0c
dongill_lee@tmax.co.kr <dongill_lee@tmax.co.kr>
2022-03-28 14:54:40 +0900

    vm 생성 api 테스트 추가

--- __test__/__admin__/__vm3__.py
+++ __test__/__admin__/__vm3__.py
@@ -3,8 +3,10 @@
 from __common__.__parameter__ import *
 from __common__.__module__ import *
 from selenium.webdriver.common.by import By
+from __common__.__ssh__ import ssh_connection
 
 from __common__.__testlink__ import *
+from __common__.__restAPI__ import *
 
 class admin_vm3:
     def __init__(self, webDriver):
@@ -12,15 +14,81 @@ class admin_vm3:
         self.webDriver = webDriver
         self._vm3Name = 'auto_vm3_'+randomString() # 랜덤 이름
         self.tl = testlink()
-          
+
+        self.rest = restAPI('https://master166.tmax.com/ovirt-engine/api/','__tools__/__rest_api__/master166_ca.crt', 'admin@internal', 'asdf')
+
+    def get_property_value_from_xml(self, xml_, tag, prop):
+        # 전체 태그 입력시, 원하는 태그의 속성을 추출
+        xml_ = xml_.split('\n')
+        tag_name = ''
+        for i in xml_:
+            if '<'+tag+' ' in i:
+                tag_name = i
+                break
+        print('tag :', tag_name)
+        if tag_name == '':
+            print("No tag")
+            return
+        else:
+            propIdx = tag_name.find(prop)
+            prop_ = tag_name[propIdx+len(prop)+2:]
+            a = prop_.find('"')
+            propValue = prop_[:a]
+            print(tag,prop,':',propValue)
+            
+            return propValue
+
+    def setup(self):
+        # 컴퓨팅
+        time.sleep(1)
+        printLog("[VM SETUP] Compute - Virtual Machines")
+        self.webDriver.findElement('id','compute', True)
+        time.sleep(1)
+
+        # 가상머신
+        self.webDriver.findElement('id','MenuView_vmsAnchor',True)
+        self.webDriver.implicitlyWait(10)
+        time.sleep(3)
+
     def test(self):
-        self.create_restapi()
+        self.createRestAPI()
 
-
-    def create_restapi(self):
+    def createRestAPI(self):
         try:
-            result = PASS
+            result = FAIL
             msg = ''
+
+            self.rest.post('vms/',
+            {'Accept':'application/xml',
+             'Content-type':'application/xml'},
+             '''<vm>
+  <name>%s</name>
+  <description>VM creation from rest api automation</description>
+  <cluster>
+    <name>Default</name>
+  </cluster>
+  <template>
+    <name>Blank</name>
+  </template>
+  <memory>536870912</memory>
+  <os>
+    <boot>
+      <devices>
+        <device>hd</device>
+      </devices>
+    </boot>
+  </os>
+</vm>'''%self._vm3Name)
+
+            self.setup()
+
+            time.sleep(10)
+
+            _createCheck = self.webDriver.tableSearch(self._vm3Name, 2, False, False, True)
+            if _createCheck[2] == self._vm3Name:
+                result = PASS
+                msg = ''
+
         except Exception as e:
             result = FAIL
             msg = str(e).replace("\n",'')
