db0b5a1c1010c32a8ea49cfa7b6a09b1a6455c87
jeonghyeon_kim <jeonghyeon_kim@tmax.co.kr>
2022-06-08 11:30:32 +0900

    Logical Network 모듈 자동화 코드 업로드
    - 편집, 삭제 코드 초안

--- __test__/__admin__/__logical_network__.py
+++ __test__/__admin__/__logical_network__.py
@@ -14,11 +14,16 @@ class admin_logical_network: # 모두 소문자
         self._logicalNetworkResult = [] # lowerCamelCase 로
         self.webDriver = webDriver
         self._networksName = 'auto_networks_'+randomString()
+        self._networksDescription = randomString()
         self.tl = testlink()
 
     def test(self):
         self.setup()
         self.create()
+        time.sleep(0.3)
+        self.edit()
+        time.sleep(0.3)
+        self.delete()
 
     def setup(self):
         # 논리 네트워크 메뉴 접근
@@ -88,20 +93,105 @@ class admin_logical_network: # 모두 소문자
         self._logicalNetworkResult.append(['networks' + DELIM + 'create' + DELIM + result + DELIM + msg])
 
         self.tl.junitBuilder('NETWORKS_CREATE', result, msg)
-
-    def sample_test_case1(self):
-
+    
+    def edit(self):
+        printLog(printSquare('Edit Networks'))
         try:
-            result = PASS
+            result = FAIL
             msg = ''
+
+            # table 내부 전부 검색해서 입력한 이름이 있을경우 클릭
+            time.sleep(3)
+            self.webDriver.tableSearch(self._networksName, 0, nameClick=True)
+
+            # 편집 클릭
+            printLog("[EDIT NETWORKS] Edit networks")
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id', 'ActionPanelView_Edit', True)
+            
+            # 설명에 임의의 문자열 입력 후 저장
+            self.webDriver.explicitlyWait(10, By.ID, 'AbstractNetworkPopupView_descriptionEditor')
+            time.sleep(3) # element 뜰 때까지 대기 필요
+            self.webDriver.findElement('id', 'AbstractNetworkPopupView_descriptionEditor', True)
+            
+            self.webDriver.sendKeys(self._networksDescription)
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id', 'AbstractNetworkPopupView_OnSave', True)
+            time.sleep(3) # element 뜰 때까지 대기 필요
+
+            # 메뉴 재접근
+            self.setup()
+
+            # 설명에 추가되면 성공
+            _editCheck = self.webDriver.tableSearch(self._networksDescription, 3)
+            printLog("[EDIT NETWORKS] Check if edited")
+            if _editCheck == True:
+                result = PASS
+                msg = ''
+            else:
+                result = FAIL
+                msg = "Failed to edit new networks..."
         except Exception as e:
             result = FAIL
             msg = str(e).replace("\n",'')
-            msg = msg[:msg.find('Element <')]
-            printLog("[SAMPLE TEST] MESSAGE : " + msg)
+            printLog("[EDIT NETWORKS] MESSAGE : " + msg)
+        printLog("[EDIT NETWORKS] RESULT : " + result)
+        self._logicalNetworkResult.append(['networks' + DELIM + 'edit' + DELIM + result + DELIM + msg])
 
-        # 결과 저장
-        printLog("[SAMPLE TEST] RESULT : " + result)
-        self._sampleResult.append(['Sample' + DELIM + 'test case 1' + DELIM + result + DELIM + msg]) # 대소문자 상관없음
+        self.tl.junitBuilder('EDIT_NETWORKS', result, msg)
 
-        self.tl.junitBuilder('SAMPLE_TEST_CASE_1',result, msg) # 모두 대문자
+    def delete(self):
+        printLog(printSquare('Delete Pools'))
+        
+        # 풀 메뉴 재진입
+        self.setup()
+
+        try:
+            result = FAIL
+            msg = ''
+
+            # table 내부 전부 검색해서 입력한 이름이 있을경우 클릭
+            time.sleep(0.5)
+            self.webDriver.tableSearch(self._networksName, 0, nameClick=True)    
+
+            # 삭제 클릭
+            printLog("[DELETE NETWORKS] Delete networks")
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id', 'ActionPanelView_Remove', True)
+            
+            # OK 클릭
+            self.webDriver.implicitlyWait(10)
+            self.webDriver.findElement('id', 'RemoveConfirmationPopupView_onRemove', True)
+            time.sleep(3) # 테이블 업데이트까지 대기 필요
+
+            # 삭제되면 성공
+            printLog("[DELETE NETWORKS] Check if deleted")
+            _startTime = time.time()
+            while True:
+                time.sleep(1)
+                try:
+                    _deleteCheck = self.webDriver.tableSearch(self._networksName, 0)
+                    if _deleteCheck == False:
+                        result = PASS
+                        msg = ''
+                        break
+                    else:
+                        printLog("[DELETE NETWORKS] Pools status is still deleted ...")
+                        _endTime = time.time()
+                        if _endTime - _startTime >= 60:
+                            printLog("[DELETE NETWORKS] Failed status changed : Timeout")
+                            result = FAIL
+                            msg = "Failed to delete new networks..."
+                            break
+                        else:
+                            continue
+                except:
+                    continue
+        except Exception as e:
+            result = FAIL
+            msg = str(e).replace("\n",'')
+            printLog("[DELETE NETWORKS] MESSAGE : " + msg)
+        printLog("[DELETE NETWORKS] RESULT : " + result)
+        self._logicalNetworkResult.append(['networks' + DELIM + 'delete' + DELIM + result + DELIM + msg])
+
+        self.tl.junitBuilder('DELETE_NETWORKS', result, msg)
