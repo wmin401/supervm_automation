f0402e0bf2a0101312a34b0a76d4d8d2cec2991f
jeonghyeon_kim <jeonghyeon_kim@tmax.co.kr>
2022-06-09 10:52:16 +0900

    Utilities 모듈 자동화 코드 업로드
    - 엔진 구성 도구 검증 초안

--- __test__/__admin__/__utilities__.py
+++ __test__/__admin__/__utilities__.py
@@ -1,41 +1,107 @@
 from __common__.__parameter__ import *
 from __common__.__module__ import *
+from __common__.__ssh__ import *
+
 from selenium.webdriver.common.by import By
 
 from __common__.__testlink__ import *
 
 '''
-    샘플파일
-    아래 코드는 필수로 사용되어야함
-    주석은 삭제해도됨(# 로 입력된 부분이나, 이 주석)
-    모든 sample 로 입력 되어있는 부분을 변경하여아함(대소문자 구분하여)
-    출력은 본인이 원하는 곳에 추가하여 사용(보통은 디버그용)
-    코드 작성은 작성자가 원하는대로 가능(함수를 나눠도 되고 하나로 해도 되고)
-
-
-    작성자 : CQA2 이동일
+    작성자 : Infra QA 김정현
 '''
 
-class admin_sample: # 모두 소문자
-    def __init__(self, webDriver):
-        self._sampleResult = [] # lowerCamelCase 로
-        self.webDriver = webDriver
-        # self._sampleName = 'auto_sample_'+randomString() # 필요할 경우 사용
+class admin_utilities: # 모두 소문자
+    def __init__(self):
+        self._utilitiesResult = [] # lowerCamelCase 로
+        self._engineConfigOutput = 'AffinityRulesEnforcementManagerEnabled'
+        self._engineConfigKeyName = 'IsIncrementalBackupSupported'
+        self._engineConfigKeyValue = 'true'
+        self._engineConfigVersion = '4.2'
+        self._engineUpStatus = 'active'
         self.tl = testlink()
 
-    def sample_test_case1(self):
+    def test(self):
+        self.engine_configuration_tool()
 
+    def engine_configuration_tool(self):
+        printLog(printSquare('Engine Configuration Tool'))
         try:
-            result = PASS
+            result = ''
             msg = ''
+            ssh = ssh_connection('192.168.17.165', 22, 'root', 'asdf')
+            ssh.activate()
+
+            printLog("[ENGINE CONFIGURATION TOOL] List option")
+            _list_option_output, _list_option_error = ssh.commandExec('engine-config --list')
+            if self._engineConfigOutput not in _list_option_output[0]:
+                result = FAIL
+                msg = _list_option_error
+            else:
+                printLog("[ENGINE CONFIGURATION TOOL] List option - OK")
+
+            printLog("[ENGINE CONFIGURATION TOOL] All option")
+            _all_option_output, _all_option_error = ssh.commandExec('engine-config --all')
+            if self._engineConfigOutput not in _all_option_output[0]:
+                result = FAIL
+                msg = _all_option_error
+            else:
+                printLog("[ENGINE CONFIGURATION TOOL] All option - OK")
+
+            printLog("[ENGINE CONFIGURATION TOOL] Get option")
+            _get_option_output, _get_option_error = ssh.commandExec('engine-config --get ' + self._engineConfigOutput)
+            if self._engineConfigOutput not in _get_option_output[0]:
+                result = FAIL
+                msg = _get_option_error
+            else:
+                printLog("[ENGINE CONFIGURATION TOOL] Get option - OK")
+
+            printLog("[ENGINE CONFIGURATION TOOL] Set option")
+            ssh.commandExec('engine-config --set ' +  self._engineConfigKeyName + '=' + self._engineConfigKeyValue + ' --cver=' + self._engineConfigVersion)
+            _set_option_output, _set_option_error = ssh.commandExec('engine-config --get ' + self._engineConfigKeyName)
+            if self._engineConfigKeyValue not in _set_option_output[0]:
+                result = FAIL
+                msg = _set_option_error
+            else:
+                printLog("[ENGINE CONFIGURATION TOOL] Set option - OK")
+
+            printLog("[ENGINE CONFIGURATION TOOL] Check applying")
+            ssh.commandExec('systemctl restart ovirt-engine.service')
+
+            _startTime = time.time()
+            while True:
+                time.sleep(1)
+                try:
+                    _endTime = time.time()
+                    if _endTime - _startTime >= 60:
+                        printLog("[ENGINE CONFIGURATION TOOL] Failed status changed : Timeout")
+                        result = FAIL
+                        msg = "Failed to apply by engine config tool..."
+                        break
+
+                    _status_check_output, _status_check_error = ssh.commandExec('systemctl status ovirt-engine.service')
+                    if self._engineUpStatus not in _status_check_output[2]:
+                        printLog("[ENGINE CONFIGURATION TOOL] Engine status is still not up...")
+                        continue
+
+                    _get_option_output, _get_option_error = ssh.commandExec('engine-config --get ' + self._engineConfigKeyName)
+                    if self._engineConfigKeyValue not in _get_option_output[0]:
+                        printLog("[ENGINE CONFIGURATION TOOL] Engine config is still not applying...")
+                        continue
+                    else:
+                        printLog("[ENGINE CONFIGURATION TOOL] Check applying - OK")
+                        break
+                except:
+                    continue
+
+            if result != FAIL:
+                result = PASS
+
+            ssh.deactivate()
         except Exception as e:
             result = FAIL
             msg = str(e).replace("\n",'')
-            msg = msg[:msg.find('Element <')]
-            printLog("[SAMPLE TEST] MESSAGE : " + msg)
+            printLog("[ENGINE CONFIGURATION TOOL] MESSAGE : " + msg)
+        printLog("[ENGINE CONFIGURATION TOOL] RESULT : " + result)
+        self._utilitiesResult.append(['engine' + DELIM + 'configuration' + DELIM + 'tool' + DELIM + result + DELIM + msg])
 
-        # 결과 저장
-        printLog("[SAMPLE TEST] RESULT : " + result)
-        self._sampleResult.append(['Sample' + DELIM + 'test case 1' + DELIM + result + DELIM + msg]) # 대소문자 상관없음
-
-        self.tl.junitBuilder('SAMPLE_TEST_CASE_1',result, msg) # 모두 대문자
+        self.tl.junitBuilder('ENGINE_CONFIGURATION_TOOL', result, msg)
